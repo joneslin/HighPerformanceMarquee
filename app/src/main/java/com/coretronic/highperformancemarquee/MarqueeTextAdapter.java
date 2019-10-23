edb6c8f57c331a7de218c1efce3bdf72be9917c1
林建宏 <jones.lin@coretronic.com>
2019-10-23 10:37:07 +0800

    jones init commit.

---
+++ app/src/main/java/com/coretronic/highperformancemarquee/MarqueeTextAdapter.java
@@ -0,0 +1,60 @@
+package com.coretronic.highperformancemarquee;
+
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+
+public class MarqueeTextAdapter extends RecyclerView.Adapter<MarqueeTextAdapter.ViewHolder> {
+
+    private List<String> dataset;
+    private float textSize;
+    private int textColor;
+    private boolean isBold;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private View view;
+        public ViewHolder(View view) {
+            super(view);
+            this.view = view;
+        }
+    }
+
+    public MarqueeTextAdapter(List<String> dataset, float textSize, int textColor, boolean isBold) {
+        this.dataset = dataset;
+        this.textSize = textSize;
+        this.textSize = textSize;
+        this.textColor = textColor;
+        this.isBold = isBold;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView tv = new TextView(parent.getContext());
+        tv.setMaxLines(1);
+        return new ViewHolder(tv);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        int widthParam = (position > 0 && position < dataset.size() - 1) ?
+                ViewGroup.LayoutParams.WRAP_CONTENT : ViewGroup.LayoutParams.MATCH_PARENT;
+
+        TextView tv = (TextView) holder.view;
+        tv.setLayoutParams(new ViewGroup.LayoutParams(widthParam, ViewGroup.LayoutParams.MATCH_PARENT));
+        tv.setText(dataset.get(position));
+        tv.setTextSize(textSize);
+        tv.setTextColor(textColor);
+        tv.setTypeface(isBold ? Typeface.DEFAULT_BOLD : Typeface.DEFAULT);
+        tv.setIncludeFontPadding(false);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+}
